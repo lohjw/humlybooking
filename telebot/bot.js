8548b44263e1d58a35b3e0675698a18f4639b8b7
SoonQuan <guansoonquan@gmail.com>
2023-10-13 16:16:46 +0800

    error handling

--- telebot/bot.js
+++ telebot/bot.js
@@ -6,7 +6,7 @@ const moment = require('moment');
 const axios = require('axios');
 const apiUrl = process.env.API_URL;
 
-function HumlyBooking(username, password, desk, startTime, endTime) {
+function HumlyBooking(toMsg, username, password, desk, startTime, endTime) {
     return axios.post(`${apiUrl}/login`, {
         username,
         password
@@ -38,7 +38,11 @@ function HumlyBooking(username, password, desk, startTime, endTime) {
 
         // book desk
         let res = await createBooking(userId, token, bookingData)
-        return res
+        if (res.status == 'success') {
+            return await bot.sendMessage(toMsg.chat.id, "Booking Completed")
+        } else {
+            return await bot.sendMessage(toMsg.chat.id, "Booking Failed. Please try again")
+        }
     }).catch((error) => {
         console.log(error);
     });
@@ -86,7 +90,7 @@ function getAllDesks(userId, authToken, queryParams) {
 }
 
 
-const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+const bot = new TelegramBot(process.env.TEST_BOT_TOKEN, { polling: true });
 
 function jsonReader(filePath, cb) {
     fs.readFile(filePath, (err, fileData) => {
@@ -102,10 +106,15 @@ function jsonReader(filePath, cb) {
     });
 }
 
+function split(str, index) {
+    const result = [str.slice(0, index), str.slice(index)];
+
+    return result;
+}
+
 bot.onText(/\/config/, async msg => {
-    const userNamePrompt = await bot.sendMessage(msg.chat.id, `Hi @${
-        msg.from.username
-    }, what's your username?`, {
+    const userNamePrompt = await bot.sendMessage(msg.chat.id, `Hi @${msg.from.username
+        }, what's your username?`, {
         reply_markup: {
             force_reply: true
         }
@@ -130,12 +139,11 @@ bot.onText(/\/config/, async msg => {
                     console.log("Error reading file:", err);
                     return;
                 }
-                users[`${
-                        msg.from.username
+                users[`${msg.from.username
                     }`] = userConfig
 
                 fs.writeFile("./users.json", JSON.stringify(users, null, 4), err => {
-                    if (err) 
+                    if (err)
                         console.log("Error writing file:", err);
                 });
             });
@@ -151,18 +159,25 @@ bot.onText(/\/book/, async msg => { // 1. Get seat number
             console.log("Error reading file:", err);
             return;
         }
-        if (users[`${
-                msg.from.username
+        if (users[`${msg.from.username
             }`]) {
-            const seatPrompt = await bot.sendMessage(msg.chat.id, `Hi @${
-                msg.from.username
-            }, Which seat are you booking?`, {
+            const booking = {
+                "username": users[`${msg.from.username
+                    }`].username,
+                "password": users[`${msg.from.username
+                    }`].password,
+                "desk": "",
+                "startTime": "",  //fromTime.format("YYYY-MM-DDTHH:mm:ssZ")
+                "endTime": ""     //toTime.format("YYYY-MM-DDTHH:mm:ssZ")
+            }
+            const seatPrompt = await bot.sendMessage(msg.chat.id, `Hi @${msg.from.username
+                }, Which seat are you booking?`, {
                 reply_markup: {
                     force_reply: true
                 }
             });
             bot.onReplyToMessage(msg.chat.id, seatPrompt.message_id, async (seatMsg) => {
-                const seat = seatMsg.text;
+                booking.desk = seatMsg.text.toLocaleUpperCase();
                 // 2. From what time
                 const fromPrompt = await bot.sendMessage(seatMsg.chat.id, "From what time?", {
                     reply_markup: {
@@ -170,46 +185,53 @@ bot.onText(/\/book/, async msg => { // 1. Get seat number
                     }
                 });
                 bot.onReplyToMessage(seatMsg.chat.id, fromPrompt.message_id, async (fromMsg) => {
-                    const fromText = fromMsg.text;
-                    // 3. To what time
-                    const toPrompt = await bot.sendMessage(fromMsg.chat.id, "To what time?", {
-                        reply_markup: {
-                            force_reply: true
-                        }
-                    });
-                    bot.onReplyToMessage(fromMsg.chat.id, toPrompt.message_id, async (toMsg) => {
-                        const toText = toMsg.text;
+                    let fromText = fromMsg.text;
+                    if (!isNaN(Number(fromText))) {
                         let fromTime = moment();
                         let toTime = moment();
                         if (fromText != "now") {
-                            fromTime = moment().set('hour', fromText).set('minute', 0);
+                            if (fromText.includes("all")) {
+                                toTime = moment().set('hour', 23).set('minute', 59);
+                                booking.startTime = fromTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                                booking.endTime = toTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                                return await HumlyBooking(fromMsg, booking.username, booking.password, booking.desk, booking.startTime, booking.endTime)
+                            } else {
+                                if (fromText.length <= 2) {
+                                    fromText = fromText + "00"
+                                }
+                                const [hour, minute] = split(fromText, -2)
+                                fromTime = fromTime.set('hour', Number(hour)).set('minute', Number(minute));
+                                booking.startTime = fromTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                            }
                         }
-                        toTime = moment().set('hour', toText).set('minute', 0)
-                        const booking = {
-                            "username": users[`${
-                                msg.from.username
-                            }`].username,
-                            "password": users[`${
-                                msg.from.username
-                            }`].password,
-                            "desk": seat,
-                            "startTime": fromTime.format("YYYY-MM-DDTHH:mm:ssZ"),
-                            "endTime": toTime.format("YYYY-MM-DDTHH:mm:ssZ")
-                        }
-                        const response = await HumlyBooking(booking.username,booking.password,booking.desk,booking.startTime,booking.endTime)
-                        // await bot.sendMessage(toMsg.chat.id, JSON.stringify(response))
-                        if (response.status == 'success') {
-                            await bot.sendMessage(toMsg.chat.id, "Booking Completed")
-                        } else {
-                            await bot.sendMessage(toMsg.chat.id, "Booking Failed. Please try again")
-                        }
-                    });
+                        // 3. To what time
+                        const toPrompt = await bot.sendMessage(fromMsg.chat.id, "To what time?", {
+                            reply_markup: {
+                                force_reply: true
+                            }
+                        });
+                        bot.onReplyToMessage(fromMsg.chat.id, toPrompt.message_id, async (toMsg) => {
+                            let toText = toMsg.text;
+                            if (!isNaN(Number(toText))) {
+                                if (toText.length <= 2) {
+                                    toText = toText + "00"
+                                }
+                                const [hour, minute] = split(toText, -2)
+                                toTime = moment().set('hour', hour).set('minute', minute)
+                                booking.endTime = toTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                                return await HumlyBooking(toMsg, booking.username, booking.password, booking.desk, booking.startTime, booking.endTime)
+                            } else {
+                                bot.sendMessage(toMsg.chat.id, "Error: Please provide time in [HH] or [HHmm]")
+                            }
+                        });
+                    } else {
+                        bot.sendMessage(fromMsg.chat.id, "Error: Please provide time in [HH] or [HHmm]")
+                    }
                 });
             });
         } else {
-            await bot.sendMessage(msg.chat.id, `Hi @${
-                msg.from.username
-            }, Please setup your account using \`/config\``)
+            await bot.sendMessage(msg.chat.id, `Hi @${msg.from.username
+                }, Please setup your account using \`/config\``)
         }
     });
 });
