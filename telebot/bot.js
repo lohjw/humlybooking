28ebc0df98f72b4dab7c065d48e24f1d7f4f29f5
SoonQuan <guansoonquan@gmail.com>
2023-11-01 15:33:49 +0800

    biweekly

--- telebot/bot.js
+++ telebot/bot.js
@@ -8,6 +8,8 @@ const apiUrl = process.env.API_URL;
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 const schedule = require('node-schedule');
+
+// hourly
 schedule.scheduleJob('0 */1 * * *', function (fireDate) {
     jsonReader("./seats.json", async (err, seatsfile) => {
         jsonReader("./users.json", async (err, usersfile) => {
@@ -27,6 +29,35 @@ schedule.scheduleJob('0 */1 * * *', function (fireDate) {
                 }
                 console.log(booking)
                 await HumlyBook(booking.username, booking.password, booking.desk, booking.startTime, booking.endTime)
+            }
+            console.log(`END => ${new Date()}}`)
+        })
+    })
+});
+
+// biweekly
+schedule.scheduleJob('0 * 1,15 * *', function () {
+    jsonReader("./seats.json", async (err, seatsfile) => {
+        jsonReader("./users.json", async (err, usersfile) => {
+            const seatings = seatsfile.seats;
+            const users = Object.values(usersfile);
+            console.log(`START => ${new Date()}`)
+            for (let i = 0; i < seatings.length; i++) {
+                const currentDateIndex = moment().isoWeekday();
+                const exclude = [6, 7, 13, 14]
+                for (let index = currentDateIndex; index < 20; index++) {
+                    if (!exclude.includes(index)) {
+    
+                        const booking = {
+                            "username": users[i % users.length].username,
+                            "password": users[i % users.length].password,
+                            "desk": seatings[i],
+                            "startTime": moment().isoWeekday(index).set('hour', 10).set('minute', 0).format("YYYY-MM-DDTHH:mm:ssZ"),  //fromTime.format("YYYY-MM-DDTHH:mm:ssZ")
+                            "endTime": moment().isoWeekday(index).set('hour', 18).set('minute', 0).format("YYYY-MM-DDTHH:mm:ssZ")     //toTime.format("YYYY-MM-DDTHH:mm:ssZ")
+                        }
+                        await HumlyBook(booking.username, booking.password, booking.desk, booking.startTime, booking.endTime)
+                    }
+                }
             }
             console.log(`END => ${new Date()}}`)
         })
@@ -309,6 +340,56 @@ bot.onText(/\/weekly/, async msg => {
                         }
                     } else {
                         for (let index = currentDateIndex; index < 6; index++) {
+                            let fromTime = moment().isoWeekday(index).set('hour', 10).set('minute', 0);
+                            let toTime = moment().isoWeekday(index).set('hour', 18).set('minute', 0);
+                            booking.startTime = fromTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                            booking.endTime = toTime.format("YYYY-MM-DDTHH:mm:ssZ");
+                            await HumlyBooking(seatMsg, booking.username, booking.password, booking.desk, booking.startTime, booking.endTime)
+                        }
+                    }
+                } catch (error) {
+                    console.log(error)
+                }
+            })
+        } else {
+            await bot.sendMessage(msg.chat.id, `Hi @${msg.from.username
+                }, Please setup your account using \`/config\``)
+        }
+    })
+});
+
+bot.onText(/\/biweekly/, async msg => {
+    jsonReader("./users.json", async (err, users) => {
+        if (err) {
+            console.log("Error reading file:", err);
+            return;
+        }
+        if (users[`${msg.from.id
+            }`]) {
+            const booking = {
+                "username": users[`${msg.from.id
+                    }`].username,
+                "password": users[`${msg.from.id
+                    }`].password,
+                "desk": "",
+                "startTime": "",  //fromTime.format("YYYY-MM-DDTHH:mm:ssZ")
+                "endTime": ""     //toTime.format("YYYY-MM-DDTHH:mm:ssZ")
+            }
+
+            const seatPrompt = await bot.sendMessage(msg.chat.id, `Hi @${msg.from.username
+                }, Which seat are you booking?`, {
+                reply_markup: {
+                    force_reply: true
+                }
+            });
+            bot.onReplyToMessage(msg.chat.id, seatPrompt.message_id, async (seatMsg) => {
+                booking.desk = seatMsg.text.toLocaleUpperCase();
+
+                try {
+                    const currentDateIndex = moment().isoWeekday();
+                    const exclude = [6, 7, 13, 14]
+                    for (let index = currentDateIndex; index < 20; index++) {
+                        if (!exclude.includes(index)) {
                             let fromTime = moment().isoWeekday(index).set('hour', 10).set('minute', 0);
                             let toTime = moment().isoWeekday(index).set('hour', 18).set('minute', 0);
                             booking.startTime = fromTime.format("YYYY-MM-DDTHH:mm:ssZ");
